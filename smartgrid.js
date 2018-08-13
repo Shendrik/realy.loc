e5e8a134841c042039499592741934e2dc60043e
Shendryk <stanislav.shendryk@gmail.com>
2018-08-13 18:21:59 +0300

    header

--- smartgrid.js
+++ smartgrid.js
@@ -3,32 +3,60 @@ const smartgrid = require('smart-grid');
 const settings = {
     columns: 24,
     offset: '10px',
+
     container: {
-        maxWidth: '960px',
+        maxWidth: '1280px',
         fields: '30px'
     },
     breakPoints: {
+        lg: {
+            width: '1200px',
+            fields: '30px'
+        },
         md: {
-            width: "992px",
-            fields: "20px"
+            width: '992px',
+            fields: '15px'
         },
         sm: {
-            width: "720px",
-            fields: "10px"
+            width: '720px',
+            fields: '15px'
         },
         xs: {
-            width: "576px",
-            fields: "5px"
+            width: '576px',
+            fields: '15px'
         },
         xxs: {
             width: "380px",
             fields: "5px"
         }
     },
+    // container: {
+    //     maxWidth: '960px',
+    //     fields: '30px'
+    // },
+    // breakPoints: {
+    //     md: {
+    //         width: "992px",
+    //         fields: "20px"
+    //     },
+    //     sm: {
+    //         width: "720px",
+    //         fields: "10px"
+    //     },
+    //     xs: {
+    //         width: "576px",
+    //         fields: "5px"
+    //     },
+    //     xxs: {
+    //         width: "380px",
+    //         fields: "5px"
+    //     }
+    // },
+    
     oldSizeStyle: false,
     properties: [
         'justify-content'
     ]
-};
+}
 
 smartgrid('./src/precss', settings);
